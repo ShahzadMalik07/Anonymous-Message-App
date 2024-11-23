6f06048850699d0a885f27f3fec428ddc78fafb9
ShahzadMalik07 <shahzadmalik425@gmail.com>
2024-11-23 22:32:02 +0530

    route changes

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -8,6 +8,19 @@ export async function POST(request: Request) {
     await DbConnect()
     try {
        const {username, email, password} = await request.json()
+       const existingUserByUsername = await UserModel.findOne({
+        username,
+        isVerified:true
+       })
+       if (existingUserByUsername) {
+        return Response.json({
+            success:false,
+            message:"Username already taken"
+        },{
+            status:400
+        })
+        
+       }
 
     } catch (error) {
         console.log("error registring user", error)
