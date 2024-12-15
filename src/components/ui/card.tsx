7f056f887137745211380c2f4b1c6322ef8440fa
ShahzadMalik07 <shahzadmalik425@gmail.com>
2024-12-15 23:47:08 +0530

    card ui updated

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex justify-between items-center space-y-1.5 p-6", className)}
+    className={cn("flex justify-between items-center space-x-4 h-[100%] p-4", className)}
     {...props}
   />
 ))
